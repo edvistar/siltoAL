18863c2e17f96e222dd4d4bafffb11dbbc396da0
victor <victorhoyoscolombia@gmail.com>
2020-05-20 19:53:13 -0500

    silto base desde leteme

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('URL', 'http://localhost/siltoWapv/');
+
+define('HOST', 'localhost');
+define('DB', 'silto');
+define('USER', 'root');
+define('PASSWORD', "");
+define('CHARSET', 'utf8');
+define("DEBUG",true);
+
+?>
