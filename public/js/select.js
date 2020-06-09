87ec1ee1c31303a3ff56fccb482bde74fe7a30ac
edvistar <victorhoyoscolombia@gmail.com>
2020-06-09 01:39:34 -0500

    selct.js

--- public/js/select.js
+++ public/js/select.js
@@ -39,7 +39,7 @@ function listarCiudades(iddepartamento){
        var cadena = "";
        if(data.length>0){
            for (var i = 0; i < data.length; i++) {
-              cadena +="<option value='"+data[i][1]+"'>"+data[i][1]+"</option>"     
+              cadena +="<option value='"+data[i][0]+"'>"+data[i][1]+"</option>"     
                
            }
            $("#ciudades").html(cadena);
