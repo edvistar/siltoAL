ca164628df036c65ee719ed5bd64eb3c883108c2
edvistar <victorhoyoscolombia@gmail.com>
2020-06-11 17:05:39 -0500

    BASE DE DATOS INNER

--- silto.sql
+++ silto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-06-2020 a las 05:19:04
+-- Tiempo de generación: 12-06-2020 a las 00:03:12
 -- Versión del servidor: 10.3.16-MariaDB
 -- Versión de PHP: 7.3.7
 
@@ -49,15 +49,15 @@ CREATE TABLE `centro` (
   `departamento` varchar(25) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Departamento de centro',
   `ciudad` varchar(25) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Ciudad de centro',
   `lugar` varchar(25) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Lugar de centro o bodega',
-  `nombreUsuario` varchar(20) COLLATE utf8_spanish2_ci NOT NULL
+  `identificacion` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `centro`
 --
 
-INSERT INTO `centro` (`id_centro`, `nombre`, `email`, `telefono`, `whatsapp`, `departamento`, `ciudad`, `lugar`, `nombreUsuario`) VALUES
-(34545, 'OcatiCotas', 'lavaca@gmail.com', 3138252764, 0, '21', '21', 'Chia', '22656626');
+INSERT INTO `centro` (`id_centro`, `nombre`, `email`, `telefono`, `whatsapp`, `departamento`, `ciudad`, `lugar`, `identificacion`) VALUES
+(34545, 'OcatiCotas', 'lavaca@gmail.com', 3138252764, 0, '21', '21', 'Chia', 22656626);
 
 -- --------------------------------------------------------
 
@@ -167,14 +167,14 @@ INSERT INTO `departamentos` (`idDepa`, `departamento`) VALUES
 CREATE TABLE `producto` (
   `id_producto` int(11) NOT NULL COMMENT 'Identificación de producto ',
   `nombre` varchar(11) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Nombre del producto',
-  `costo` int(11) NOT NULL COMMENT 'Valor por kilo de producto'
+  `peso` int(11) NOT NULL COMMENT 'peso de producto'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
-INSERT INTO `producto` (`id_producto`, `nombre`, `costo`) VALUES
+INSERT INTO `producto` (`id_producto`, `nombre`, `peso`) VALUES
 (2, 'carlosss', 25000),
 (6, 'carlos', 4500),
 (78, 'Manzana', 567);
@@ -199,6 +199,14 @@ CREATE TABLE `rutas` (
   `id_producto` int(11) NOT NULL COMMENT 'id de producto',
   `id_solicitud` int(11) NOT NULL COMMENT 'id de solicitud'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `rutas`
+--
+
+INSERT INTO `rutas` (`id_ruta`, `fecha_ruta`, `hora_salida`, `hora_llegada`, `descripcion`, `tipo_ruta`, `precinto`, `identificacion`, `placa`, `id_centro`, `id_producto`, `id_solicitud`) VALUES
+(1, '2020-06-11', '0000-00-00', '0000-00-00', 'Carga especial', 'Nacional', '123456', 20199375, 'AD343', 34545, 78, 1),
+(2, '2020-06-11', '0000-00-00', '0000-00-00', 'Carga especial', 'Local', '1234567', 1683809522, 'AD343', 34545, 78, 4);
 
 -- --------------------------------------------------------
 
@@ -234,6 +242,16 @@ CREATE TABLE `solicitud` (
   `identificacion` bigint(20) NOT NULL COMMENT 'Nombre del encargado de centro.'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `solicitud`
+--
+
+INSERT INTO `solicitud` (`id_solicitud`, `solicitud`, `descripcion`, `id_centro`, `identificacion`) VALUES
+(1, '2020-06-09 00:00:00', 'viaje completo', 34545, 22656626),
+(2, '2020-06-11 20:37:13', 'Viaje commpleto', 0, 20199375),
+(3, '2020-06-11 20:50:11', 'Viaje commpleto', 34545, 1683809522),
+(4, '2020-06-11 23:15:17', 'Viaje commpleto', 34545, 22656626);
+
 -- --------------------------------------------------------
 
 --
@@ -242,11 +260,11 @@ CREATE TABLE `solicitud` (
 
 CREATE TABLE `usuario` (
   `identificacion` bigint(20) NOT NULL COMMENT 'Identificacion de user',
-  `nombreUsuario` varchar(25) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Nombre de user',
-  `apellidoUsuario` varchar(25) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Apellido  de user',
-  `emailUsuario` varchar(50) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Email de user',
-  `telefonoUsuario` int(20) NOT NULL COMMENT 'Numero de celular',
-  `whatsappUsuario` tinyint(1) NOT NULL COMMENT 'Whatsapp',
+  `nombre` varchar(25) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Nombre de user',
+  `apellido` varchar(25) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Apellido  de user',
+  `email` varchar(50) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Email de user',
+  `telefono` int(20) NOT NULL COMMENT 'Numero de celular',
+  `whatsapp` tinyint(1) NOT NULL COMMENT 'Whatsapp',
   `cargo` varchar(15) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Rol de user',
   `estado` text COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Estado del User',
   `fecha_ingreso` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Fecha de ingreso user',
@@ -258,7 +276,8 @@ CREATE TABLE `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`identificacion`, `nombreUsuario`, `apellidoUsuario`, `emailUsuario`, `telefonoUsuario`, `whatsappUsuario`, `cargo`, `estado`, `fecha_ingreso`, `foto`, `pass`) VALUES
+INSERT INTO `usuario` (`identificacion`, `nombre`, `apellido`, `email`, `telefono`, `whatsapp`, `cargo`, `estado`, `fecha_ingreso`, `foto`, `pass`) VALUES
+(20199375, 'Patricia lara', 'Lara Dimate', 'lida@gmail.com', 2147483647, 1, 'Seleccionar', 'activo', '2020-06-11 06:45:03', '', '8459d70c344917c311aeac9216969e3b'),
 (22656626, 'Victor Eduardo', 'Hoyos Sandoval', 'victorhoyoscolombia@gmail.com', 2147483647, 1, 'coordinador', 'activo', '2020-06-09 21:46:40', 'public/img/contact/', 'fcea920f7412b5da7be0cf42b8c93759'),
 (1683809522, 'luis', 'joder', 'luisjoder@gmail.com', 2123252764, 1, 'supervisor', 'activo', '2020-06-22 00:00:00', '', '1234567');
 
@@ -277,7 +296,7 @@ CREATE TABLE `vehiculo` (
   `costo_flete` int(11) NOT NULL COMMENT 'Costo de plete por carga',
   `gps` tinyint(4) NOT NULL COMMENT 'GPS de vehiculo',
   `estado` varchar(11) COLLATE utf8_spanish2_ci NOT NULL COMMENT 'Estao de vehiculo en la empresa propiedad o contratista.',
-  `conductor` bigint(20) NOT NULL COMMENT 'Nombre de conductor.',
+  `identificacion` bigint(20) NOT NULL COMMENT 'Nombre de conductor.',
   `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Fecha del registro'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
@@ -285,10 +304,8 @@ CREATE TABLE `vehiculo` (
 -- Volcado de datos para la tabla `vehiculo`
 --
 
-INSERT INTO `vehiculo` (`placa`, `capacidad`, `seguro`, `tecnomecanica`, `tipo_vehiculo`, `costo_flete`, `gps`, `estado`, `conductor`, `fecha_registro`) VALUES
-('678ty', '15 tonelads', 'hgfdsa', '2020-06-05', 'contratista', 4545555, 0, 'furgon', 1070007809, '2020-06-13 05:00:00'),
-('ghy56', '15 tonelads', 'hgfdsa', '2020-06-27', 'contratista', 4545555, 0, 'furgon', 10783, '2020-06-26 05:00:00'),
-('yui32', '7', 'hgfdsa', '2020-06-25', 'contratista', 4545555, 0, 'furgon', 77878787887, '2020-06-25 05:00:00');
+INSERT INTO `vehiculo` (`placa`, `capacidad`, `seguro`, `tecnomecanica`, `tipo_vehiculo`, `costo_flete`, `gps`, `estado`, `identificacion`, `fecha_registro`) VALUES
+('AD343', '10', 'Omega', '2020-06-11', 'Furgon', 200000, 0, 'excelente', 1683809522, '2020-06-11 05:00:00');
 
 --
 -- Índices para tablas volcadas
@@ -322,12 +339,7 @@ ALTER TABLE `producto`
 -- Indices de la tabla `rutas`
 --
 ALTER TABLE `rutas`
-  ADD PRIMARY KEY (`id_ruta`),
-  ADD UNIQUE KEY `placa` (`placa`),
-  ADD UNIQUE KEY `id_centro` (`id_centro`),
-  ADD UNIQUE KEY `id_solicitud` (`id_solicitud`),
-  ADD UNIQUE KEY `id_producto` (`id_producto`),
-  ADD UNIQUE KEY `identificacion` (`identificacion`);
+  ADD PRIMARY KEY (`id_ruta`);
 
 --
 -- Indices de la tabla `sectores`
@@ -339,9 +351,7 @@ ALTER TABLE `sectores`
 -- Indices de la tabla `solicitud`
 --
 ALTER TABLE `solicitud`
-  ADD PRIMARY KEY (`id_solicitud`),
-  ADD UNIQUE KEY `id_centro` (`id_centro`),
-  ADD UNIQUE KEY `identificacion` (`identificacion`);
+  ADD PRIMARY KEY (`id_solicitud`);
 
 --
 -- Indices de la tabla `usuario`
@@ -366,10 +376,16 @@ ALTER TABLE `departamentos`
   MODIFY `idDepa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
+-- AUTO_INCREMENT de la tabla `rutas`
+--
+ALTER TABLE `rutas`
+  MODIFY `id_ruta` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificación de ruta', AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de la tabla `solicitud`
 --
 ALTER TABLE `solicitud`
-  MODIFY `id_solicitud` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id de la solicitud para la ruta.';
+  MODIFY `id_solicitud` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id de la solicitud para la ruta.', AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
