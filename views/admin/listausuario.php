dfb3e331e335355fb4a5ebf32b62e9e94c192d11
jacc89 <laflacalex9@gmail.com>
2020-06-18 21:39:39 -0500

    pdf usuario

--- views/admin/listausuario.php
+++ views/admin/listausuario.php
@@ -25,7 +25,8 @@ if ($_SESSION['cargo']!="supervisor") {
                         <div id="main">
                             <div><?php echo $this->mensaje; ?></div>
                             <h1 class="text-center"><i class="fa fa-users" aria-hidden="true"></i> Listado de Usuarios</h1>
-                            <button type="button" class="btn btn-success loginbtn" onClick='window.location.assign("<?php echo constant('URL'); ?>/usuario/crear")'>Crear Usuario</button><br>
+                            <button type="button" class="btn btn-success loginbtn" onClick='window.location.assign("<?php echo constant('URL'); ?>usuario/crear")'>Crear Usuario</button>
+                            <button type="button" class="btn btn-success loginbtn" onClick='window.location.assign("<?php echo constant('URL'); ?>public/pdf/usuario.php")'>PDF</button><br>
                             <table id="tabla" class="table table-hover">
                                 <thead class="thead-dark">
                                     <tr>
